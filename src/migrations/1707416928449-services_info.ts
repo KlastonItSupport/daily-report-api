611552b3dc07033f493162368673b47a4128967f
gustavool1 <go8895806@gmail.com>
2024-02-08 15:34:29 -0300

    feat: adding service info migration

---
+++ src/migrations/1707416928449-services_info.ts
@@ -0,0 +1,80 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class ServicesInfo1707416928449 implements MigrationInterface {
+  private TABLE_NAME = 'services_info';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.TABLE_NAME,
+        columns: [
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          { name: 'id', type: 'varchar', isPrimary: true },
+          {
+            name: 'professional_name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'professional_email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'client_name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'client_email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'start_date',
+            type: 'date',
+            isNullable: false,
+          },
+          {
+            name: 'end_date',
+            type: 'date',
+            isNullable: false,
+          },
+          {
+            name: 'service_type',
+            type: 'tinyint',
+            isNullable: true,
+          },
+          {
+            name: 'service_price',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'executed_service',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'pendencies',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'planning',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.TABLE_NAME);
+  }
+}
