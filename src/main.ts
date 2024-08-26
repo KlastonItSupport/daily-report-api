fe34362fac7a65201638b26b2e72300104c8d995
gustavool1 <go8895806@gmail.com>
2024-08-26 15:18:26 -0300

    feat: setting up for pm2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { dataSource } from './database';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  let httpsOptions = {};
+
+  if (process.env.ENVIRONMENT === 'prod') {
+    console.log('Using prod certs');
+    httpsOptions = {
+      key: fs.readFileSync(process.env.SELF_SIGNED_KEY_PATH),
+      cert: fs.readFileSync(process.env.SELF_SIGNED_CRT_PATH),
+    };
+  } else {
+    console.log('Using dev certs');
+  }
+  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions });
 
   await dataSource.initialize();
   await dataSource.runMigrations();
