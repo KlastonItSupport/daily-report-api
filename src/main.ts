0816cce562421fc99a7d2ee9fbb48deed6cfabe4
gustavool1 <go8895806@gmail.com>
2024-08-30 10:16:56 -0300

    feat: testing https

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { dataSource } from './database';
+import * as bodyParser from 'body-parser';
+import 'reflect-metadata';
 import * as fs from 'fs';
 import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
 
 dotenv.config();
-async function bootstrap() {
-  let httpsOptions = {};
+console.log('Starting...');
 
+async function bootstrap() {
+  Logger.log('Using prod xxxxx');
+
+  const httpsOptions = {};
   if (process.env.ENVIRONMENT === 'prod') {
     console.log('Using prod certs');
-    httpsOptions = {
-      key: fs.readFileSync(process.env.SELF_SIGNED_KEY_PATH),
-      cert: fs.readFileSync(process.env.SELF_SIGNED_CRT_PATH),
-    };
+    // httpsOptions = {
+    //   key: fs.readFileSync(process.env.SELF_SIGNED_KEY_PATH),
+    //   cert: fs.readFileSync(process.env.SELF_SIGNED_CRT_PATH),
+    // };
   } else {
     console.log('Using dev certs');
   }
-  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions });
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   await dataSource.initialize();
   await dataSource.runMigrations();
 
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT);
 }
 bootstrap();
